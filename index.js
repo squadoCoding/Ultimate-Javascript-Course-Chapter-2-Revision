91b0d6a4fbb3b6f8ed7986971ed54413eef306dd
squadoCoding <106444206+squadoCoding@users.noreply.github.com>
2022-11-05 13:41:03 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,62 @@
+// Chapter2 -Expressions and Conditions
+/* Expression is a piece of code that produces a value, every value written literally is an expression and it can also become a javascript program in itself. */
+
+// Operators:- These are symbols that help us to perform operations on values (data).
+// 1.Arithematical operators
+// let a = 3;
+// let b = 5;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+// console.log(a ** b);
+// console.log(a++);// it will increment the value of a after printing a to the console.
+// console.log(++b);// it will increment the value of b before printing it to the console.
+// console.log(a--);// it will decrement the value of a after printing a to the console.
+// console.log(--b);// it will decrement the value of b before printing it to the console.
+
+// 2.Assignment Operators
+// let a = 23;
+// a += 2;
+// a -= 3;
+// a /= 2;
+// a *= 2;
+// a **= 2;
+// a %= 2;
+// console.log(a);
+
+// 3.Comparision/Relational Operators
+// let a = 34;
+// let b = 3;
+
+// console.log(a == b);
+// console.log(a > b);
+// console.log(a < b);
+// console.log(a >= b);
+// console.log(a <= b);
+// console.log(a != b);
+// b = "34";
+// console.log(a === b);
+// console.log(a == b ? "they are equal" : "they are not equal");
+// console.log(a === b ? "they are equal" : "they are not equal");
+
+// 4.Logical operators
+// let a = 3;
+// let b = "Mumbai";
+
+// console.log((a==b)&&(a==3));
+// console.log((a==b)||(a==3));
+// console.log(!(a==b));
+
+// Apart from above operators we also have bitwise and type operators in Javasscript
+// The values on which the operator is used are called operands and the value produced by operator us called result.
+
+// We have the following conditional statements in Javascrpt
+/*
+1.if statement
+2.if else statement
+3.if else lader
+4.switch case statement
+*/
